d5d1e42f617b4c71eb9d02d24f2dd978653b910b
Jacky Tung <jackytung20@gmail.com>
2023-04-17 22:24:08 -0400

    created input validator and test cases for adding/editing daily#

---
+++ PLANer/app/src/main/java/com/example/planer/InputValidator.java
@@ -0,0 +1,13 @@
+package com.example.planer;
+
+public class InputValidator {
+
+    static boolean validateTitleAndGoalNumber(String title, Integer goalNumber){
+        return true;
+    }
+
+    static boolean validateDailyNumber(Integer dailyNumber){
+        return true;
+    }
+
+}
